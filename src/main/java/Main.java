0d53d32700f2fb0bd4216e9802625eeb3a4ed854
Jonathan <joas4713@student.dsv.se>
2020-09-15 22:23:13 +0200

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
 public class Main {
+
+    private String name;
+    private int age;
+
+    public Main(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
